94c1fb838702050518b5b42f69797be87304f01f
V0xt <alex.5363@ya.ru>
2019-10-13 12:21:24 +0300

    Quicksort algorithm realisation

---
+++ 04_quicksort/04_quicksort.js
@@ -0,0 +1,22 @@
+quicksort = (array) => {
+    if (array.length < 2) {
+      return array;
+    }
+    else {
+      let pivot = array[0];
+      let less = [];
+      let greater = [];  
+      let arr = array.slice(1);      
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] <= pivot) {
+          less[i] = arr[i];
+        }
+        else {
+          greater[i] = arr[i];
+        }
+      }
+      return quicksort(less).concat(pivot, quicksort(greater));
+    }
+  }
+  
+  console.log(quicksort([10, 5, 2, 3]));
