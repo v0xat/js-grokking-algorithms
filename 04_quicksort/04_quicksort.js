d3ffb5140e0d52314117c1fb831c4ab946d1c5f3
V0xt <alex.5363@ya.ru>
2019-10-13 12:28:12 +0300

    Refactor

--- 04_quicksort/04_quicksort.js
+++ 04_quicksort/04_quicksort.js
@@ -1,18 +1,18 @@
 quicksort = (array) => {
     if (array.length < 2) {
-      return array;
+        return array;
     }
     let pivot = array[0];
     let less = [];
     let greater = [];  
     let arr = array.slice(1);      
     for (let i = 0; i < arr.length; i++) {
-    if (arr[i] <= pivot) {
-        less[i] = arr[i];
-    }
-    else {
-        greater[i] = arr[i];
-    }
+        if (arr[i] <= pivot) {
+            less[i] = arr[i];
+        }
+        else {
+            greater[i] = arr[i];
+        }
     }
     return quicksort(less).concat(pivot, quicksort(greater));    
   }
