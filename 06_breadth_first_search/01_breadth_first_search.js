7b56a4689140a7dc84be141ca3664d2e035669c3
Mona Lisa <alex.5363@ya.ru>
2020-04-03 19:38:56 +0300

    Bread first search

---
+++ 06_breadth_first_search/01_breadth_first_search.js
@@ -0,0 +1,34 @@
+const person_is_seller = (name) => {
+	return name[name.length - 1] === 'm';
+};
+
+const graph = {};
+graph['you'] = ['alice', 'bob', 'claire'];
+graph['bob'] = ['anuj', 'peggy'];
+graph['alice'] = ['peggy'];
+graph['claire'] = ['thom', 'jonny'];
+graph['anuj'] = [];
+graph['peggy'] = [];
+graph['thom'] = [];
+graph['jonny'] = [];
+
+const search = (name) => {
+	let search_queue = [];
+	search_queue = search_queue.concat(graph[name]);
+	const searched = [];
+	while (search_queue.length) {
+		const person = search_queue.shift();
+		if (searched.indexOf(person) === -1) {
+			if (person_is_seller(person)) {
+				console.log(person + ' is a mango seller!');
+				return true;
+			}
+
+			search_queue = search_queue.concat(graph[person]);
+			searched.push(person);
+		}
+	}
+	return false;
+};
+
+search('you');
